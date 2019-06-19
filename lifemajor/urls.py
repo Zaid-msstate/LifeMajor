18b9d257d47056de44d7c4d508f60cbfe671248b
Jonathan Friedman <jonbenfri@gmail.com>
2019-06-19 18:41:48 -0400

    makes students app main project app

--- lifemajor/urls.py
+++ lifemajor/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('students/', include('students.urls')),
+    path('', include('students.urls')),
     path('admin/', admin.site.urls),
 ]
