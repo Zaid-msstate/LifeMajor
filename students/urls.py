969c67f5449febc0533d3ac59d98efa68e6c6cfb
Jonathan Friedman <jonbenfri@gmail.com>
2019-06-20 09:40:31 -0400

    adds new template

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
